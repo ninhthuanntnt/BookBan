f8655f4853cdc5b7c0b3b73bc9daa281e7e64619
Nguyễn Thuận <ninhthuan1999@gmail.com>
2020-07-16 14:17:47 +0700

    Them Layout

--- BookBan/Form1.cs
+++ BookBan/Form1.cs
@@ -16,5 +16,20 @@ namespace BookBan
         {
             InitializeComponent();
         }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button24_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
